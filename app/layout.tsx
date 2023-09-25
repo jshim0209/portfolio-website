03f41b42ff10def607ad04271d9a0abd58c7f3ba
jshim0209 <100379451+jshim0209@users.noreply.github.com>
2023-09-25 12:16:45 -0400

    navbar header & intro section added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from "@/components/header";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -16,7 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-gray-50 text-gray-950 relative`}>
+      <body
+        className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px]
+        pt-28 sm:p-36`}
+      >
         <div
           className="bg-[#fbe2e3] absolute 
         top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] 
@@ -28,6 +32,7 @@ export default function RootLayout({
         rounded-full blur-[10rem] sm:w-[68.75rem] 
         md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
         ></div>
+        <Header />
         {children}
       </body>
     </html>
--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "ntvb.tmsimg.com",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
